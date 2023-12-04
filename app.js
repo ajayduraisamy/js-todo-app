de6d259cfe80dc708fceb49bec2f7ad8f25e5905
Ajay Duraisamy <ajayduraisamy@gmail.com>
2023-12-04 10:00:00 +0530

    Enhanced todo app with edit, delete, localStorage and premium UI

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-let tasks = [];
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function saveToLocal() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 function addTask() {
     const input = document.getElementById("taskInput");
     const task = input.value.trim();
 
-    if(task === "") return;
+    if (task === "") return;
 
     tasks.push(task);
     input.value = "";
 
+    saveToLocal();
     render();
 }
 
@@ -18,18 +23,44 @@ function render() {
 
     tasks.forEach((task, index) => {
         const li = document.createElement("li");
-        li.textContent = task + " ";
-        
-        const btn = document.createElement("button");
-        btn.innerText = "Done";
-        btn.onclick = () => removeTask(index);
 
-        li.appendChild(btn);
+        const span = document.createElement("span");
+        span.innerText = task;
+
+        // Edit button
+        const editBtn = document.createElement("button");
+        editBtn.innerText = "Edit";
+        editBtn.className = "edit";
+        editBtn.onclick = () => editTask(index);
+
+        // Delete button
+        const delBtn = document.createElement("button");
+        delBtn.innerText = "Delete";
+        delBtn.className = "delete";
+        delBtn.onclick = () => removeTask(index);
+
+        li.appendChild(span);
+        li.appendChild(editBtn);
+        li.appendChild(delBtn);
         list.appendChild(li);
     });
 }
 
-function removeTask(index){
-    tasks.splice(index,1);
+function removeTask(index) {
+    tasks.splice(index, 1);
+    saveToLocal();
     render();
 }
+
+function editTask(index) {
+    const updatedTask = prompt("Edit task:", tasks[index]);
+
+    if (updatedTask !== null && updatedTask.trim() !== "") {
+        tasks[index] = updatedTask.trim();
+        saveToLocal();
+        render();
+    }
+}
+
+// Load tasks on startup
+render();
